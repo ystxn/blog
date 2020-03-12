ee0c61b081561ef3e73861a824139eccc7f2fa2a
Yong Sheng Tan <yong.tan@symphony.com>
2020-03-12 21:56:13 +0800

    Revised blog post style

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import "./blog-post.css"
+import "./blog-post.scss"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
