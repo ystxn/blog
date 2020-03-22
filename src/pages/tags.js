db158f4bdbe1e92c168003641939d0ce1e35b127
Yong Sheng Tan <tanys@outlook.sg>
2020-03-22 13:43:13 +0800

    Sort tags by total count

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -6,14 +6,16 @@ import SEO from "../components/seo"
 export default ({ data, location }) => {
   const { title, description } = data.site.siteMetadata
   const group = data.allMarkdownRemark.group
-  const tags = group.map(tag =>
-    <li key={tag.fieldValue}>
-      <Link key={tag.fieldValue} to={`/tags/${tag.fieldValue.replace(/ /g, '-')}/`}>
-        #{tag.fieldValue}
-      </Link>
-      {` (${tag.totalCount})`}
-    </li>
-  )
+  const tags = group
+    .sort((a, b) => b.totalCount - a.totalCount)
+    .map(tag =>
+      <li key={tag.fieldValue}>
+        <Link key={tag.fieldValue} to={`/tags/${tag.fieldValue.replace(/ /g, '-')}/`}>
+          #{tag.fieldValue}
+        </Link>
+        {` (${tag.totalCount})`}
+      </li>
+    )
 
   return (
     <Layout location={location} title={title} subTitle={description}>
