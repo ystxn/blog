f606b3f72341b7cb8c1a2ac5fe6c01c86f1b5e72
Yong Sheng Tan <tanys@outlook.sg>
2020-04-05 23:34:54 +0800

    Added scroll to top button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,38 +1,67 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { CSSTransition } from 'react-transition-group';
 import Bio from "../components/bio"
 import { Link } from "gatsby"
 import "./layout.scss"
 
-const NavBar = () =>
-    <nav className='main-nav'>
-      <ol>
-        <li>
-          <Link to="/">./home</Link>
-        </li>
-        <li>
-          <Link to="/about">./about</Link>
-        </li>
-        <li>
-          <Link to="/tags">./tags</Link>
-        </li>
-      </ol>
-    </nav>
+const NavBar = () => (
+  <nav className="main-nav">
+    <ol>
+      <li>
+        <Link to="/">./home</Link>
+      </li>
+      <li>
+        <Link to="/about">./about</Link>
+      </li>
+      <li>
+        <Link to="/tags">./tags</Link>
+      </li>
+    </ol>
+  </nav>
+)
 
-const SubTitle = ({subTitle}) => subTitle ? <h5>{subTitle}</h5> : ''
+const SubTitle = ({ subTitle }) => (subTitle ? <h5>{subTitle}</h5> : "")
 
-const Layout = ({ title, subTitle, children }) =>
-  <div className='envelope'>
-    <header className='main-header'>
-      <h2>
-        <Link to={`/`}>{title}</Link>
-      </h2>
-      <SubTitle subTitle={subTitle} />
-    </header>
-    <NavBar />
-    <main>{children}</main>
-    <footer>
-      <Bio />
-    </footer>
-  </div>
+const Layout = ({ title, subTitle, children }) => {
+  const [ scrolled, setScrolled ] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  });
+  const onScroll = () => setScrolled(window.scrollY > 200)
+  const scrollToTop = () => window.scrollTo(0, 0)
 
+  return (
+    <div className="envelope">
+      <header className="main-header">
+        <h2>
+          <Link to={`/`}>{title}</Link>
+        </h2>
+        <SubTitle subTitle={subTitle} />
+      </header>
+      <NavBar />
+      <main>{children}</main>
+      <footer>
+        <Bio />
+      </footer>
+
+      <CSSTransition
+        in={scrolled}
+        timeout={400}
+        classNames="display"
+        unmountOnExit
+      >
+        <div
+          role="button"
+          tabIndex={0}
+          onClick={scrollToTop}
+          onKeyDown={scrollToTop}
+          className={`scrollButton`}
+        >
+          ^
+        </div>
+      </CSSTransition>
+    </div>
+  )
+}
 export default Layout
