bb2780fd2d7e4cd04169728cf8f93afeb29b439b
Yong Sheng Tan <yong.tan@symphony.com>
2020-04-06 13:22:24 +0800

    Separated scroll-button scss

--- src/components/scroll-button.js
+++ src/components/scroll-button.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { CSSTransition } from 'react-transition-group';
+import "./scroll-button.scss"
 
 const ScrollButton = () => {
     const [ scrolled, setScrolled ] = useState(false);
